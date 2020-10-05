a41a9ebb9e0d8776058d9c8c0a77cfef84861720
Ryan Pelavin <rpelavin@gmail.com>
2020-10-05 19:12:50 -0400

    fleshing out script more

--- Test.ps1
+++ Test.ps1
@@ -1,5 +1,11 @@
 dotnet run -p .\SirilCmd\ `
-    "cd 'c:\StackBot\Multimedia\Working Files_JS\2020-8-25 Stackbot Example\Bias'" `
+    "cd 'c:\StackBot\Multimedia\Working Files_JS\2020-8-25 Stackbot Example'" `
+    "cd bias" `
     "convertraw 2240Bias_" `
     "setext fits" `
-    "stack bias_ rej 3 3 -nonorm"
+    "stack 2240Bias_ rej 3 3 -nonorm" `
+    "cd .." `
+    "cd flat"`
+    "convertraw 2044NewFlat_" `
+    "preprocess 2044NewFlat_ -bias=../bias/2240Bias_stacked" `
+    "stack 2044NewFlat_ rej 3 3 -norm=mul"
