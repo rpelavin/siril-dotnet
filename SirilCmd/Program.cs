76a5e6ab3c02fe042b1f01ce3f21b9fc922a0843
Ryan Pelavin <rpelavin@gmail.com>
2020-10-05 16:29:37 -0400

    simplifying

--- SirilCmd/Program.cs
+++ SirilCmd/Program.cs
@@ -4,83 +4,21 @@
     using System.IO;
     using System.IO.Pipes;
     using System.Security.Principal;
-    using System.Text;
-    using System.Threading;
 
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            var pipeClient = new NamedPipeClientStream(
+            var stream = new NamedPipeClientStream(
                 ".",
                 "siril_command.out",
                 PipeDirection.In,
                 PipeOptions.None,
                 TokenImpersonationLevel.Impersonation);
-
-            Console.WriteLine("Connecting to server...\n");
-            pipeClient.Connect();
-
-            var ss = new StreamString(pipeClient);
-            // Validate the server's signature string.
-            var outString = ss.ReadString();
-            if (ss.ReadString() == "I am the one true server!")
+            stream.Connect();
+            using (var streamReader = new StreamReader(stream))
             {
-                // The client security token is sent with the first write.
-                // Send the name of the file whose contents are returned
-                // by the server.
-                ss.WriteString("c:\\textfile.txt");
-
-                // Print the file to the screen.
-                Console.Write(ss.ReadString());
-            }
-            else
-            {
-                Console.WriteLine("Server could not be verified.");
-            }
-            pipeClient.Close();
-            // Give the client process some time to display results before exiting.
-            Thread.Sleep(4000);
-        }
-
-        // Defines the data protocol for reading and writing strings on our stream
-        public class StreamString
-        {
-            private Stream ioStream;
-            private UnicodeEncoding streamEncoding;
-
-            public StreamString(Stream ioStream)
-            {
-                this.ioStream = ioStream;
-                streamEncoding = new UnicodeEncoding();
-            }
-
-            public string ReadString()
-            {
-                int len = 0;
-
-                len = ioStream.ReadByte() * 256;
-                len += ioStream.ReadByte();
-                byte[] inBuffer = new byte[len];
-                ioStream.Read(inBuffer, 0, len);
-
-                return streamEncoding.GetString(inBuffer);
-            }
-
-            public int WriteString(string outString)
-            {
-                byte[] outBuffer = streamEncoding.GetBytes(outString);
-                int len = outBuffer.Length;
-                if (len > UInt16.MaxValue)
-                {
-                    len = (int)UInt16.MaxValue;
-                }
-                ioStream.WriteByte((byte)(len / 256));
-                ioStream.WriteByte((byte)(len & 255));
-                ioStream.Write(outBuffer, 0, len);
-                ioStream.Flush();
-
-                return outBuffer.Length + 2;
+                Console.WriteLine(streamReader.ReadToEnd());
             }
         }
     }
