4ca4a2c377a5885b89e74748e78218b0da6346c3
Ryan Pelavin <rpelavin@gmail.com>
2020-10-05 16:10:32 -0400

    fixing pipe direction to get it working

--- SirilCmd/Program.cs
+++ SirilCmd/Program.cs
@@ -14,33 +14,33 @@
             var pipeClient = new NamedPipeClientStream(
                 ".",
                 "siril_command.out",
-                PipeDirection.InOut,
+                PipeDirection.In,
                 PipeOptions.None,
                 TokenImpersonationLevel.Impersonation);
 
-                Console.WriteLine("Connecting to server...\n");
-                pipeClient.Connect();
+            Console.WriteLine("Connecting to server...\n");
+            pipeClient.Connect();
 
-                var ss = new StreamString(pipeClient);
-                // Validate the server's signature string.
-                var outString = ss.ReadString();
-                if (ss.ReadString() == "I am the one true server!")
-                {
-                    // The client security token is sent with the first write.
-                    // Send the name of the file whose contents are returned
-                    // by the server.
-                    ss.WriteString("c:\\textfile.txt");
+            var ss = new StreamString(pipeClient);
+            // Validate the server's signature string.
+            var outString = ss.ReadString();
+            if (ss.ReadString() == "I am the one true server!")
+            {
+                // The client security token is sent with the first write.
+                // Send the name of the file whose contents are returned
+                // by the server.
+                ss.WriteString("c:\\textfile.txt");
 
-                    // Print the file to the screen.
-                    Console.Write(ss.ReadString());
-                }
-                else
-                {
-                    Console.WriteLine("Server could not be verified.");
-                }
-                pipeClient.Close();
-                // Give the client process some time to display results before exiting.
-                Thread.Sleep(4000);
+                // Print the file to the screen.
+                Console.Write(ss.ReadString());
+            }
+            else
+            {
+                Console.WriteLine("Server could not be verified.");
+            }
+            pipeClient.Close();
+            // Give the client process some time to display results before exiting.
+            Thread.Sleep(4000);
         }
 
         // Defines the data protocol for reading and writing strings on our stream
