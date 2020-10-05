6e9ec5bdc1cf51f8c74bd8a8247d2f31e6a76df0
Ryan Pelavin <rpelavin@gmail.com>
2020-10-05 19:42:55 -0400

    finishing script

--- Test.ps1
+++ Test.ps1
@@ -8,4 +8,9 @@ dotnet run -p .\SirilCmd\ `
     "cd flat"`
     "convertraw 2044NewFlat_" `
     "preprocess 2044NewFlat_ -bias=../bias/2240Bias_stacked" `
-    "stack 2044NewFlat_ rej 3 3 -norm=mul"
+    "stack 2044NewFlat_ rej 3 3 -norm=mul" `
+    "cd .." `
+    "cd dark" `
+    "convertraw 2240Dark_" `
+    "stack 2240Dark_ rej 3 3 -nonorm" `
+    "cd .."
