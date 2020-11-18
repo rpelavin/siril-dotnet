16ce59d0984e316a31d82a89e187dca70da7e1b9
Ryan Pelavin <rpelavin@gmail.com>
2020-11-18 16:35:04 -0500

    including dark masters file

--- Lights.ps1
+++ Lights.ps1
@@ -1,7 +1,7 @@
 dotnet run `
-    "cd 'c:\StackBot\Multimedia\LIGHT'" `
+    "cd 'C:\StackBot\Multimedia\Working Files_NPM\2020-10-12thru 2020-10-19 Tucson Astro\2020-10-18 together5shots2do200 410c\LIGHT'" `
     "cd 1_1" `
     "convertraw 1_1_0.25s_100g_" `
-    "preprocess 1_1_0.25s_100g_ [-dark='this file does not exist'] -cfa -equalize_cfa -debayer -stretch" `
+    "preprocess 1_1_0.25s_100g_ '-dark=C:\StackBot\Multimedia\Working Files_JS\2020-8-25 Stackbot Example\Dark\2240Dark_stacked' -cfa -equalize_cfa -debayer -stretch" `
     "cd .." `
     "exit"
