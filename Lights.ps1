f941552dc3b0eb007f76169ced0201dfa1ea75f7
Ryan Pelavin <rpelavin@gmail.com>
2020-11-19 09:34:12 -0500

    adding register and stack commands

--- Lights.ps1
+++ Lights.ps1
@@ -2,6 +2,8 @@ dotnet run `
     "cd 'C:\StackBot\Multimedia\Working Files_NPM\2020-10-12thru 2020-10-19 Tucson Astro\2020-10-18 together5shots2do200 410c\LIGHT'" `
     "cd 1_1" `
     "convertraw 1_1_0.25s_100g_" `
-    "preprocess 1_1_0.25s_100g_ '-dark=C:\StackBot\Multimedia\Working Files_JS\2020-8-25 Stackbot Example\Dark\2240Dark_stacked' -cfa -equalize_cfa -debayer -stretch" `
+    "preprocess 1_1_0.25s_100g_ -cfa -equalize_cfa -debayer -stretch" `
+    "register 1_1_0.25s_100g_" `
+    "stack 1_1_0.25s_100g_ rej 3 3 -norm=addscale -out=Panel1_exposure1" `
     "cd .." `
     "exit"
